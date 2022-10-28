d2a464b61abefc98ad15b314d0f7c489548b5a38
Valentin Kao <valentin.kao@unabiz.com>
2022-10-28 12:53:35 +0800

    edit (v0.1) copy current timestamp

---
+++ LauncherApplication/LauncherApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LauncherApplicationApp.swift
+//  LauncherApplication
+//
+//  Created by valentin.kao on 2022/10/28.
+//
+
+import SwiftUI
+
+@main
+struct LauncherApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ timestamp-status-bar/timestamp-status-bar/StatusBarController.swift
@@ -0,0 +1,8 @@
+//
+//  StatusBarController.swift
+//  timestamp-status-bar
+//
+//  Created by valentin.kao on 2022/10/28.
+//
+
+import Foundation
